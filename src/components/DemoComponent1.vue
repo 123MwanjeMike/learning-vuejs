8078307257f95a845fef486c8e650eff6622e8e8
Mwanje Mike <mwanjemike767@gmail.com>
2020-08-06 11:59:16 +0300

    first attempt

---
+++ src/components/DemoComponent1.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>This is Mike's first vue app</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DemoComponent1',
+}
+</script>
+
+<style>
+
+</style>
